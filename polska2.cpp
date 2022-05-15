f5b1704934d66a3750d9aefd31553b12a6d7aa0f
kinderror <87951557+kinderror@users.noreply.github.com>
2022-05-15 22:08:56 +0300

    Update polska2.cpp

--- polska2.cpp
+++ polska2.cpp
@@ -33,23 +33,44 @@ struct Stack {
         delete top;
         top = p;
     }
+    // pushes value or calculates
+    void calc(string val) {
+        if (top == nullptr or top->next == nullptr) {
+            push(stoi(val));
+            return;
+        }
+        int a = top->value, b = top->next->value;
+        if (val == "+") {
+            pop();
+            pop();
+            push(a + b);
+        } else if (val == "-") {
+            pop();
+            pop();
+            push(b - a);
+        } else if (val == "*") {
+            pop();
+            pop();
+            push(a * b);
+        } else if (val == "/") {
+            pop();
+            pop();
+            push(b / a);
+        } else {
+            push(stoi(val));
+        }
+    }    
 };
 
-// example of using all functions
 int main() {
-    Stack stack = Stack(); // create stack {1 2 3 4 5 6 7}
-    for (int i = 0; i < 7; i++) {
-        stack.push(i + 1);
+    Stack stack = Stack(); 
+    string val;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> val;
+        stack.calc(val);
     }
-    cout << "Empty? "; // Empty? No
-    if (stack.empty()) {
-        cout << "Yes" << endl;
-    } else {
-        cout << "No" << endl;
-    }
-    stack.pop();
-    cout << "Top element after pop(): " << stack.get() << endl; // 6
-    stack.push(57);
-    cout << "Top element after push(): " << stack.get() << endl; // 57
+    cout << stack.get();
     return 0;
 }
