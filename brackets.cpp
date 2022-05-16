fd268aab85abfde78e13448ce59f7eae3a709649
kinderror <87951557+kinderror@users.noreply.github.com>
2022-05-16 19:48:08 +0300

    Create brackets.cpp

---
+++ brackets.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Node {
+    string value;
+    Node *next;
+    Node(string val) {
+        value = val;
+        next = nullptr;
+    }
+}; 
+
+struct Stack {
+    Node *top;
+    Stack() {
+        top = nullptr;
+    }
+    // gets value of the top element
+    string get() {
+        return top->value;
+    }
+    // adds new element on top
+    void push(string val) {
+        Node *p = new Node(val);
+        p->next = top;
+        top = p;
+    }
+    // deletes the top element
+    void pop() {
+        Node *p = &(*(top->next));
+        delete top;
+        top = p;
+    }
+    // checks if brackets are right
+    bool check(string val) {
+        if (val == "(") {
+            push(val);
+        } else if (val == ")") {
+            if (top == nullptr) {
+                return false;
+            } else if (get() == "(") {
+                pop();
+                return true;
+            } else {
+                return false;
+            }
+        } else if (val == "{") {
+            push(val);
+        } else if (val == "}") {
+            if (top == nullptr) {
+                return false;
+            } else if (get() == "{") {
+                pop();
+                return true;
+            } else {
+                return false;
+            }
+        } else if (val == "[") {
+            push(val);
+        } else if (val == "]") {
+            if (top == nullptr) {
+                return false;
+            } else if (get() == "[") {
+                pop();
+                return true;
+            } else {
+                return false;
+            }
+        } 
+    }
+};
+
+int main() {
+    Stack stack = Stack();
+    string val;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> val;
+        if (not stack.check(val)) {
+            cout << -1;
+            return 0;
+        }
+    }
+    cout << 0;
+    return 0;
+}
