4774219d4e0f89f71d66280d3661b39d544698b5
kinderror <87951557+kinderror@users.noreply.github.com>
2022-05-17 01:01:25 +0300

    Update navar.cpp

--- navar.cpp
+++ navar.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 
-void least_coin_up(int *coins, int coins_size, int *optimals, int P) {
+using namespace std;
+
+void calc_optimals(int *prices, int *optimals, int L, int n) {
     optimals[0] = 0;
-    for (int sum = 1; sum != P + 1; sum++) {
-        int local_min = numeric_limits<int>::max();
-        for (int coin_idx = 0; coin_idx != coins_size; coin_idx++) {
-            if (sum >= coins[coin_idx]) {
-                local_min = min(optimals[sum - coins[coin_idx]] + 1, local_min);
+    for (int i = 1; i < L + 1; i++) {
+        int max_price = 0;
+        for (int j = 1; j < n + 1; j++) {
+            if (i >= j) {
+                max_price = max(optimals[i - j] + prices[j], max_price);
             }
         }
-        optimals[sum] = local_min;
+        optimals[i] = max_price;
     }
 }
+
 int main() {
-    
+    int L, n;
+    cin >> L >> n; // n = 10
+    int *prices = new int[n + 1]; // 1 5 8 9 10 17 17 20 24 30
+    int *optimals = new int[L + 1];
+    for (int i = 1; i < n + 1; i++) {
+        cin >> prices[i];
+    }
+    calc_optimals(prices, optimals, L, n);
+    cout << optimals[L];
     return 0;
 }
