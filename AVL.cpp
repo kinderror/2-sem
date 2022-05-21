018bea58878da579fce72451f8d19e3b3d477966
kinderror <87951557+kinderror@users.noreply.github.com>
2022-05-21 14:25:06 +0300

    Update AVL.cpp

--- AVL.cpp
+++ AVL.cpp
@@ -21,54 +21,55 @@ struct AVL_Tree {
         if (p != nullptr) bfactor = height(p->right) - height(p->left);
         return bfactor;
     }
-    // 
-
-    void fix_height (Node *z) {
-        int hl = height(z->left);
-        int hr = height(z->right);
-        z->height = std::max(hl, hr) + 1;
+    // replaces height with new value
+    void fix_height(Node *p) {
+        int hl = height(p->left);
+        int hr = height(p->right);
+        p->height = std::max(hl, hr) + 1;
     }
-
-    Node * rotateright (Node *z) {
-        Node *q = z->left;
-        z->left = q->right;
-        q->right = z;
-        fix_height(z);
+    // rotates sub_tree to the right 
+    Node *rotateright(Node *p) {
+        Node *q = p->left;
+        p->left = q->right;
+        q->right = p;
+        fix_height(p);
         fix_height(q);
         return q;
     }
-
-    Node * rotateleft (Node *z) {
-        Node *q = z->right;
-        z->right = q->left;
-        q->left = z;
-        fix_height(z);
+    // rotates sub_tree to the left
+    Node *rotateleft(Node *p) {
+        Node *q = p->right;
+        p->right = q->left;
+        q->left = p;
+        fix_height(p);
         fix_height(q);
         return q;
-    }
-
-    Node * balance (Node *z) {
-        fix_height(z);
-        if( bfactor(z) == 2) {
-            if( bfactor(z->right) < 0) z->right = rotateright(z->right);
-            return rotateleft(z);
+    } 
+    // balances tree
+    Node *balance(Node *p) {
+        fix_height(p);
+        if (bfactor(p) == 2) {
+            if (bfactor(p->right) < 0) p->right = rotateright(p->right);
+            return rotateleft(p);
         }
-        if( bfactor(z) == -2) {
-            if( bfactor(z->left) > 0) z->left = rotateleft(z->left);
-            return rotateright(z);
+        if (bfactor(p) == -2) {
+            if (bfactor(p->left) > 0) p->left = rotateleft(p->left);
+            return rotateright(p);
         }
-        return z;
+        return p;
     }
-
-    Node * rec_insert (Node *par, Node *z) { //вспомогательная функция для вставки
-        if (par == NIL) return z;
-        if (z->key < par->key) par->left = rec_insert(par->left, z);
-        else par->right = rec_insert(par->right, z);
+    // helpful func for insertion 
+    Node *rec_insert(Node *par, Node *p) { 
+        if (par == nullptr) return p;
+        if (p->key < par->key) {
+            par->left = rec_insert(par->left, p);
+        } else {
+            par->right = rec_insert(par->right, p);
         return balance(par);
     }
-
-    void insert (Node *z) { //сама вставка
-        root = rec_insert(root, z);
+    // inserts node with pointer p
+    void insert(Node *p) { 
+        root = rec_insert(root, p);
     }
 
     Node * search (Node *z, int key) { // поиск в ветке с родителем z по ключу key
@@ -76,31 +77,33 @@ struct AVL_Tree {
         if (key < z->key) return search(z->left, key);
         else return search(z->right, key);
     }
-
-    Node * fmin (Node *z) { //поиск min max в ветке с родителем z
-        while (z->left != NIL) z = z->left;
-        return z;
+    // finds node with min key in subtree
+    Node *fmin(Node *p) { //поиск min max в ветке с родителем z
+        while (p->left != nullptr) p = p->left;
+        return p;
     }
-
-    Node * fmax (Node *z) {
-        while (z->right != NIL) z = z->right;
-        return z;
+    // finds node with max key in subtree 
+    Node *fmax(Node *p) {
+        while (p->right != nullptr) p = p->right;
+        return p;
     }
-
-    Node * removemin (Node *z) { //вспомогательная функция для удаления
-        if (z->left == NIL) return z->right;
-        z->left = removemin(z->left);
-        return balance(z);
+    // helpful func for removing
+    Node *removemin(Node *p) {
+        if (p->left == nullptr) return p->right;
+        p->left = removemin(p->left);
+        return balance(p);
     }
-
-    Node * rec_remove (Node *par, Node *z) { //вспомогательная функция для удаления
-        if (par == NIL) return NIL;
-        if (z->key < par->key) par->left = rec_remove(par->left, z);
-        else if (z->key > par->key) par->right = rec_remove(par->right, z);
-        else {
+    // helpful func for removing
+    Node *rec_remove(Node *par, Node *p) { 
+        if (par == nullptr) return nullptr;
+        if (p->key < par->key) 
+            par->left = rec_remove(par->left, p);
+        } else if (p->key > par->key) {
+            par->right = rec_remove(par->right, p);
+        } else {
             Node *pl = par->left;
             Node *pr = par->right;
-            if (pr == NIL) return pl;
+            if (pr == nullptr) return pl;
             Node *min = fmin(pr);
             min->right = removemin(pr);
             min->left = pl;
@@ -108,20 +111,9 @@ struct AVL_Tree {
         }
         return balance(par);
     }
-
-    void remove (Node *z) { //удоление
-        root = rec_remove(root, z);
-    }
-
-    void print(Node *z, int level)  //вывод дерева(в 1 столбике 1 уровень, во втором-второй и тд). При несбалансированном дереве может быть не оч красивым, но структуру сохранит
-    {
-        if(z != NIL)
-        {
-            print(z->left,level + 1);
-            for(int i = 0;i< level;i++) std::cout << "   ";
-            std::cout << z->key << '\n';
-            print(z->right,level + 1);
-        }
+    // removes node with pointer p
+    void remove(Node *p) { 
+        root = rec_remove(root, p);
     }
 };
 
